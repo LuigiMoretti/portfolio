57fc3cafe3a4043a396e379b5b1952b923a604b9
Luigi Moretti <luigimoretti98@gmail.com>
2022-03-08 20:09:56 +0100

    slide pagine

--- script/script.js
+++ script/script.js
@@ -28,92 +28,90 @@ cambiaPagina(0);
 function cambiaPagina(p) {
     switch (p) {
         case 0:
-            document.getElementById("home").hidden = false;
+            $("#home").slideDown();
             document.getElementById("btnHome").classList.add("active");
 
-            document.getElementById("competenze").hidden = true;
+            $("#competenze").slideUp();
             document.getElementById("btnComp").classList.remove("active");
 
-            document.getElementById("curriculum").hidden = true;
+            $("#curriculum").slideUp();
             document.getElementById("btnCurr").classList.remove("active");
 
-            document.getElementById("progetti").hidden = true;
+            $("#progetti").slideUp();;
             document.getElementById("btnProg").classList.remove("active");
 
-            document.getElementById("contatti").hidden = true;
+            $("#contatti").slideUp();
             document.getElementById("btnCont").classList.remove("active");
             break;
 
 
         case 1:
-            document.getElementById("home").hidden = true;
+            $("#home").slideUp();
             document.getElementById("btnHome").classList.remove("active");
 
-            document.getElementById("competenze").hidden = false;
+            $("#competenze").slideDown();
             document.getElementById("btnComp").classList.add("active");
 
-            document.getElementById("curriculum").hidden = true;
+            $("#curriculum").slideUp();
             document.getElementById("btnCurr").classList.remove("active");
 
-            document.getElementById("progetti").hidden = true;
+            $("#progetti").slideUp();
             document.getElementById("btnProg").classList.remove("active");
 
-            document.getElementById("contatti").hidden = true;
+            $("#contatti").slideUp();
             document.getElementById("btnCont").classList.remove("active");
             break;
 
         case 2:
-            document.getElementById("home").hidden = true;
+            $("#home").slideUp();
             document.getElementById("btnHome").classList.remove("active");
 
-            document.getElementById("competenze").hidden = true;
+            $("#competenze").slideUp();
             document.getElementById("btnComp").classList.remove("active");
 
-            document.getElementById("progetti").hidden = false;
+            $("#progetti").slideDown();
             document.getElementById("btnProg").classList.add("active");
 
-            document.getElementById("curriculum").hidden = true;
+            $("#curriculum").slideUp();
             document.getElementById("btnCurr").classList.remove("active");
 
-            document.getElementById("contatti").hidden = true;
+            $("#contatti").slideUp();
             document.getElementById("btnCont").classList.remove("active");
             break;
 
 
         case 3:
-            document.getElementById("home").hidden = true;
+            $("#home").slideUp();
             document.getElementById("btnHome").classList.remove("active");
 
-            document.getElementById("competenze").hidden = true;
+            $("#competenze").slideUp();
             document.getElementById("btnComp").classList.remove("active");
 
-
-            document.getElementById("progetti").hidden = true;
+            $("#progetti").slideUp();
             document.getElementById("btnProg").classList.remove("active");
 
-            document.getElementById("curriculum").hidden = false;
+            $("#curriculum").slideDown();
             document.getElementById("btnCurr").classList.add("active");
 
-            document.getElementById("contatti").hidden = true;
+            $("#contatti").slideUp();
             document.getElementById("btnCont").classList.remove("active");
             break;
 
         case 4:
-            document.getElementById("home").hidden = true;
+            $("#home").slideUp();
             document.getElementById("btnHome").classList.remove("active");
 
-            document.getElementById("competenze").hidden = true;
+            $("#competenze").slideUp();
             document.getElementById("btnComp").classList.remove("active");
 
-            document.getElementById("curriculum").hidden = true;
-            document.getElementById("btnCurr").classList.remove("active");
-
-            document.getElementById("progetti").hidden = true;
+            $("#progetti").slideUp();
             document.getElementById("btnProg").classList.remove("active");
 
-            document.getElementById("contatti").hidden = false;
+            $("#curriculum").slideUp();
+            document.getElementById("btnCurr").classList.remove("active");
+
+            $("#contatti").slideDown();
             document.getElementById("btnCont").classList.add("active");
-            break;
         default:
             break;
     }
