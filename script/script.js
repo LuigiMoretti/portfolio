e4cffbab2ddc53695bfd9f7bbe61fb2b8572760a
Luigi Moretti <luigimoretti98@gmail.com>
2022-04-01 23:53:11 +0200

    modifiche al js per smartphone

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
+cambiaPagina("home");
 
 
-cambiaPagina(0);
+
+
+
 let classList = $('main.progetti').attr("class").split(/\s+/);
 if (!classList.includes("cliccato")) {
     $('main.progetti').click(function () {
@@ -25,113 +28,17 @@ $("#chiudi").on("click", function () {
 $("main").on("mouseenter", function () {
     $("main").removeClass("riposiziona");
 });
+
+
+
 function cambiaPagina(p) {
     if ($(window).width() < 768) {
-
-        switch (p) {
-            case 0:
-                $("#home").slideDown();
-                document.getElementById("btnHome").classList.add("active");
-
-                $("#competenze").slideUp();
-                document.getElementById("btnComp").classList.remove("active");
-
-                $("#curriculum").slideUp();
-                document.getElementById("btnCurr").classList.remove("active");
-
-                $("#progetti").slideUp();;
-                document.getElementById("btnProg").classList.remove("active");
-
-                $("#contatti").slideUp();
-                document.getElementById("btnCont").classList.remove("active");
-                break;
-
-
-            case 1:
-                $("#home").slideUp();
-                document.getElementById("btnHome").classList.remove("active");
-
-                $("#competenze").slideDown();
-                document.getElementById("btnComp").classList.add("active");
-
-                $("#curriculum").slideUp();
-                document.getElementById("btnCurr").classList.remove("active");
-
-                $("#progetti").slideUp();
-                document.getElementById("btnProg").classList.remove("active");
-
-                $("#contatti").slideUp();
-                document.getElementById("btnCont").classList.remove("active");
-                break;
-
-            case 2:
-                $("#home").slideUp();
-                document.getElementById("btnHome").classList.remove("active");
-
-                $("#competenze").slideUp();
-                document.getElementById("btnComp").classList.remove("active");
-
-                $("#progetti").slideDown();
-                document.getElementById("btnProg").classList.add("active");
-
-                $("#curriculum").slideUp();
-                document.getElementById("btnCurr").classList.remove("active");
-
-                $("#contatti").slideUp();
-                document.getElementById("btnCont").classList.remove("active");
-                break;
-
-
-            case 3:
-                $("#home").slideUp();
-                document.getElementById("btnHome").classList.remove("active");
-
-                $("#competenze").slideUp();
-                document.getElementById("btnComp").classList.remove("active");
-
-                $("#progetti").slideUp();
-                document.getElementById("btnProg").classList.remove("active");
-
-                $("#curriculum").slideDown();
-                document.getElementById("btnCurr").classList.add("active");
-
-                $("#contatti").slideUp();
-                document.getElementById("btnCont").classList.remove("active");
-                break;
-
-            case 4:
-                $("#home").slideUp();
-                document.getElementById("btnHome").classList.remove("active");
-
-                $("#competenze").slideUp();
-                document.getElementById("btnComp").classList.remove("active");
-
-                $("#progetti").slideUp();
-                document.getElementById("btnProg").classList.remove("active");
-
-                $("#curriculum").slideUp();
-                document.getElementById("btnCurr").classList.remove("active");
-
-                $("#contatti").slideDown();
-                document.getElementById("btnCont").classList.add("active");
-                break;
-
-            default:
-                break;
-        }
+        $("main").slideUp();
+        $(".nav-link").removeClass("active");
+        $("#" + p).slideDown();
+        document.getElementById("btn" + p).classList.add("active");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
